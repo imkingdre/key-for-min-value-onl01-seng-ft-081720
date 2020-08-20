39fd2a9943c856a3da736f95e88a82b94e9ffbed
Andre King <andreking1031@gmail.com>
2020-08-20 19:58:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,9 +9,7 @@ def key_for_min_value(name_hash)
       current_minimum_val = value 
       current_key_assoc_w_min_val = key
     else
-      if current_minimum_val > value 
-        current_minimum_val = value
-        current_key_assoc_w_min_val = key
+      if current_minimum_val > value current_minimum_val = value current_key_assoc_w_min_val = key
     end
   end
 end
